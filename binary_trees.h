523770d0027e0625173739dc86bc00d3400bdca3
Michael Bonyo <bonyomichael406@gmail.com>
2023-03-02 06:13:22 -0800

    in-order traversal of binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,6 @@
 #define BINARY_TREES_H
 
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -30,5 +29,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
