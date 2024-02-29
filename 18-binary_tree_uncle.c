a20b0b3a3cc983c78571f41c4e5082fb0b4bcbbf
maiko254 <bonyomichael406.com>
2024-02-29 19:03:41 +0300

    correction

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,15 +9,17 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent == NULL || node->parent->parent == NULL || node == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent == node->parent->parent->left
+	if (node->parent->parent->left != NULL
 		&& node->parent->parent->right != NULL)
-		return (node->parent->parent->right);
-	else if (node->parent == node->parent->parent->right
-		&& node->parent->parent->left != NULL)
-		return (node->parent->parent->left);
-	else
-		return (NULL);
+	{
+		if (node->parent->parent->left == node->parent)
+			return (node->parent->parent->right);
+		else if (node->parent->parent->right == node->parent)
+			return (node->parent->parent->left);
+	}
+
+	return (NULL);
 }
